c498e19cb94efcdb84b3f14307793b304f3fc792
Furkan Abay <furkan_abay@hotmail.com>
2025-04-02 18:41:03 +0200

    feat:name changes

--- examples/user.go
+++ examples/user.go
@@ -1,7 +1,7 @@
 package examples
 
 import (
-	"github.com/invopop/jsonschema/examples/nested"
+	"github.com/furre-dev/jsonschema-nullable/examples/nested"
 )
 
 // User is used as a base to provide tests for comments.
--- id_test.go
+++ id_test.go
@@ -3,7 +3,7 @@ package jsonschema_test
 import (
 	"testing"
 
-	"github.com/invopop/jsonschema"
+	"github.com/furre-dev/jsonschema-nullable"
 	"github.com/stretchr/testify/assert"
 )
 
